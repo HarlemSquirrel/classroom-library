0b96b8a7ee8f2628652061f1f9857146b1cb24c4
Kevin McCormack <wittyman37@yahoo.com>
2016-02-12 21:27:51 -0500

    move helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -246,16 +246,3 @@ class ApplicationController < Sinatra::Base
     redirect to '/books'
   end
 end
-
-
-
-
-class Helpers
-  def self.current_user(session)
-    User.find(session[:id]) if session[:id]
-  end
-
-  def self.is_logged_in?(session)
-    session[:id] != nil && !!self.current_user(session)
-  end
-end
